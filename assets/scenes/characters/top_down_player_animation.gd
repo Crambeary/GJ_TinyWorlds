2a698a80dc0c0bac5057a0097bea13ec77f5a257
Marc <marctotaro94@gmail.com>
2025-05-19 09:41:12 -0500

    feat: player idle based off stop direction

--- assets/scenes/characters/top_down_player_animation.gd
+++ assets/scenes/characters/top_down_player_animation.gd
@@ -1,16 +1,30 @@
 extends AnimatedSprite2D
 
 var move_vec: Vector2
+var last_move_vec: Vector2 = Vector2(0.0, 1.0)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	match move_vec:
 		Vector2(-1.0, 0.0):
 			play("move_left")
+			last_move_vec = Vector2(-1.0, 0.0)
 		Vector2(1.0, 0.0):
 			play("move_right")
+			last_move_vec = Vector2(1.0, 0.0)
 		Vector2(0.0, -1.0):
 			play("move_up")
+			last_move_vec = Vector2(0.0, -1.0)
 		Vector2(0.0, 1.0):
 			play("move_down")
-	#play("idle_down")
+			last_move_vec = Vector2(0.0, 1.0)
+		Vector2(0.0, 0.0):
+			match last_move_vec:
+				Vector2(-1.0, 0.0):
+					play("idle_left")
+				Vector2(1.0, 0.0):
+					play("idle_right")
+				Vector2(0.0, -1.0):
+					play("idle_up")
+				Vector2(0.0, 1.0):
+					play("idle_down")
 			
