12cfe4d1b31e0e2ee82d99bf382eccae6b435420
Marc <marctotaro94@gmail.com>
2025-05-20 10:21:21 -0500

    feat: priest NPC vidion cone faces direction of movement

--- assets/scenes/characters/priest.gd
+++ assets/scenes/characters/priest.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 var player: Node2D
 @onready var vision_manager: VisionManager = $VisionManager
-@onready var waypoints: Array = get_tree().get_first_node_in_group("Priest1").get_children()
+@export var path_group_name: String = "Priest1"
+@onready var waypoints: Array = get_tree().get_first_node_in_group(path_group_name).get_children()
 var current_index: int = 0
 enum STATE {PATROL, WAIT, CHASE, RETURN}
 @export var current_state = STATE.PATROL
@@ -21,15 +22,46 @@ func handle_waypoint_action(wp):
 			current_state = STATE.PATROL
 		_:
 			pass
-		
+
+func convert_string_to_direction(direction: Vector2) -> VisionManager.DIRECTIONS:
+	match direction:
+		Vector2.RIGHT:
+			return VisionManager.DIRECTIONS.RIGHT
+		Vector2.LEFT:
+			return VisionManager.DIRECTIONS.LEFT
+		Vector2.UP:
+			return VisionManager.DIRECTIONS.UP
+		Vector2.DOWN:
+			return VisionManager.DIRECTIONS.DOWN
+	return VisionManager.DIRECTIONS.LEFT
+	
+func flip_direction(direction: Vector2) -> Vector2:
+	match direction:
+		Vector2.RIGHT:
+			return Vector2.LEFT
+		Vector2.LEFT:
+			return Vector2.RIGHT
+		Vector2.UP:
+			return Vector2.DOWN
+		Vector2.DOWN:
+			return Vector2.UP
+	return Vector2.RIGHT
+	
+
+func set_vision_direction(direction: Vector2) -> void:
+	#vision_manager.front_direction = convert_string_to_direction(direction)
+	#vision_manager.look_at(global_position + direction)
+	direction = direction * -1 # Invert Vector2 Values
+	vision_manager.rotation = direction.angle()
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
 	var target = get_current_waypoint().global_position
-	var dir = (target - global_position).normalized()
+	var dir: Vector2 = (target - global_position).normalized()
 	if current_state == STATE.PATROL:
+		set_vision_direction(dir)
 		position += dir * speed * delta
 	
 		if global_position.distance_to(target) < 1:
@@ -42,3 +74,5 @@ func _physics_process(delta: float) -> void:
 	if player:
 		var player_global_pos: Vector2 = player.global_position
 		var player_in_sight = vision_manager.can_see_point(player_global_pos)
+		if player_in_sight:
+			print("See Player")
